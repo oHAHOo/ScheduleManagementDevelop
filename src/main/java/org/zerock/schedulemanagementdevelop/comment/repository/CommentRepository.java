584fb0dc2741eb5717a2b14470bae87b204b46aa
hjh <hjh690@naver.com>
2026-04-20 11:20:00 +0900

    fix : 쿼리 메서드명 수정

--- src/main/java/org/zerock/schedulemanagementdevelop/comment/repository/CommentRepository.java
+++ src/main/java/org/zerock/schedulemanagementdevelop/comment/repository/CommentRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
-    List<Comment> findByAllScheduleId(Long scheduleId);
+    List<Comment> findBySchedule_Id(Long scheduleId);
 }
